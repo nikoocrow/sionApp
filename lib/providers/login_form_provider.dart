01f43175212560b33b63c4f0bc3daf7892de1e98
Nicolas Castro <nikoocrow@gmail.com>
2022-11-04 12:54:02 -0500

    finalizacion login preparando login backend

---
+++ lib/providers/login_form_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginFormProvider extends ChangeNotifier{
+
+  GlobalKey<FormState> formkey = new  GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;  
+  set isLoading(bool value){
+    _isLoading = value;
+    notifyListeners();
+  }
+
+
+
+
+  bool isValidForm(){
+
+    print('$email - $password ');
+    return formkey.currentState?.validate() ?? false;
+  }
+
+
+
+
+}
--- lib/widgets/card_login_container.dart
+++ lib/widgets/card_login_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sion_app/providers/login_form_provider.dart';
 import 'package:sion_app/ui/inputs_decorations.dart';
+import 'package:provider/provider.dart';
 
 
 class CardLoginContainerWidget extends StatelessWidget {
@@ -22,10 +24,13 @@ class CardLoginContainerWidget extends StatelessWidget {
           const SizedBox(height: 30),
 
 
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: _Formulario(),
-          )
+
+            ChangeNotifierProvider(
+              create: ( _ ) => LoginFormProvider() ,
+              child: _Formulario(),
+            )  
+            
+          
         ],
       
      ),
@@ -36,91 +41,127 @@ class CardLoginContainerWidget extends StatelessWidget {
 class _Formulario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-         //CAMPO DE TEXTO USUARIO
-        Container(
-    
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child:TextField( 
-                autocorrect: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecorations.authInputDecorations(
-                  hintText: 'usuario@correoelectronico.com',
-                  labelText: 'Email'
+
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
+    return Form(
+      key: loginForm.formkey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children: [
+          
+             //CAMPO DE TEXTO USUARIO
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child:TextFormField( 
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecorations.authInputDecorations(
+                      hintText: 'usuario@correoelectronico.com',
+                      labelText: 'Email'
+                    ),
+                    onChanged: (value) => loginForm.email = value,
+                    validator: (value){
+                      String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                      RegExp regExp  = new RegExp(pattern);
+                      return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El email digitado no luce adecuado';
+                    },
+                    
                 ),
+                
+              ),
             ),
-          ),
-        ),
-
-        const SizedBox(height: 15),
-
-  //CAMPO DE  CONTRASEÑA
-
-  //CAMPO DE TEXTO USUARIO
-        Container(
-    
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child:TextField( 
-                autocorrect: false,
-                obscureText: true,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecorations.authInputDecorations(
-                  hintText:  'ingresa ID de la iglesia',
-                  labelText: 'Contraseña'
+          
+            const SizedBox(height: 15),
+          
+        //CAMPO DE  CONTRASEÑA
+          
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child:TextFormField( 
+                    autocorrect: false,
+                    obscureText: true,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecorations.authInputDecorations(
+                      hintText:  'ingresa ID de la iglesia',
+                      labelText: 'Contraseña'
+                    ),
+                    onChanged: (value) => loginForm.password = value,
+                    validator: (value) {
+                      return (value != null && value.length >=6)
+                      ? null
+                      :'La contraseña debe ser el ID de la iglesia';
+                    },
+                    ),
                 ),
-                ),
-            ),
-          ),
-        
-
-         const SizedBox(height: 30),
-
-         Container(
-          decoration: BoxDecoration(
-            boxShadow:[
-              BoxShadow(
-                color: Colors.black12,
-                spreadRadius: 1,
-                blurRadius: 10,
-               
-              )
-
-            ]
-          ),
-           child: MaterialButton(
-             
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            disabledColor: Colors.grey,
-            color: Color(0xFF00AAFF),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-              child: Text(
-                'INGRESAR',
-                style: TextStyle(color: Colors.white),
-                )
-            ),
-            onPressed: (){
-              Navigator.pushNamed(context, 'home');
-            }
+              ),
             
-                 ),
-         )
+          
+             const SizedBox(height: 30),
+          
+             Container(
+              decoration: BoxDecoration(
+                boxShadow:[
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                   
+                  )
+          
+                ]
+              ),
+               child: MaterialButton(
+                 
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                disabledColor: Colors.grey,
+                color: Color(0xFF00AAFF),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text(
+                    loginForm.isLoading
+                    ? 'Espera'
+                    : 'Ingresar',
+                    style: TextStyle(color: Colors.white),
+                    )
+                ),
+                onPressed: loginForm.isLoading ? null : () async{
 
-        
-      ],
+                  FocusScope.of(context).unfocus();
+                  loginForm.isValidForm();
+                  if(!loginForm.isValidForm()) return;
+
+                 loginForm.isLoading = true;
+                 await Future.delayed(Duration(seconds: 2));
+                 //TODO VALIDAR SI EL LOGIN ES CORRECTO
+                 loginForm.isLoading = false;
+
+                 Navigator.pushReplacementNamed(context, 'home');
+                }
+                
+                     ),
+             )
+          
+            
+          ],
+        ),
+      ),
     );
   }
 }
